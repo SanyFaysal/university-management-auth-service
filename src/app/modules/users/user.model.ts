bb6cd7fa26f871588bbbf1888790f116bbe52db5
Abu Sani Faysal <tfoysalahmed@gmail.com>
2023-06-03 20:09:57 +0600

    done : user schema interface model

---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,28 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './user.interface'
+
+type UserModal = Model<IUser, object>
+
+const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = model<IUser, UserModal>('User', userSchema)
+export default User
